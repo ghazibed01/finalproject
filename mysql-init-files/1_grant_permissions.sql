a6cabda215e9695c178d8b1725fb57ab9b9e9617
ghazibed01 <93162410+ghazibed01@users.noreply.github.com>
2025-07-07 21:42:35 +0100

    Update 1_grant_permissions.sql

--- mysql-init-files/1_grant_permissions.sql
+++ mysql-init-files/1_grant_permissions.sql
@@ -1,10 +1,13 @@
 -- Supprime les anciens privilèges (optionnel)
 REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'root'@'%';
 
--- Applique tous les droits au root depuis toutes les IPs
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;
+-- Met à jour le mot de passe du root
+ALTER USER 'root'@'%' IDENTIFIED BY 'root';
 
--- Supprime toutes les limites de ressource et contraintes SSL
+-- Applique tous les droits au root depuis toutes les IPs
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+-- Supprime toutes les limites de ressources et contraintes SSL
 ALTER USER 'root'@'%' REQUIRE NONE
 WITH MAX_QUERIES_PER_HOUR 0
      MAX_CONNECTIONS_PER_HOUR 0
