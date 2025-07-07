3a9cfbdd623d49505de006b31356395c02f8f529
ghazibed01 <93162410+ghazibed01@users.noreply.github.com>
2025-07-07 21:57:54 +0100

    Update 1_grant_permissions.sql

--- mysql-init-files/1_grant_permissions.sql
+++ mysql-init-files/1_grant_permissions.sql
@@ -1,18 +1,19 @@
--- Supprime les anciens privilèges (optionnel)
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'root'@'%';
+-- Crée l'utilisateur si nécessaire
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root';
 
--- Met à jour le mot de passe du root
-ALTER USER 'root'@'%' IDENTIFIED BY 'root';
-
--- Applique tous les droits au root depuis toutes les IPs
+-- Donne tous les privilèges à root@'%'
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
--- Supprime toutes les limites de ressources et contraintes SSL
+-- Supprime les limitations
 ALTER USER 'root'@'%' REQUIRE NONE
 WITH MAX_QUERIES_PER_HOUR 0
      MAX_CONNECTIONS_PER_HOUR 0
      MAX_UPDATES_PER_HOUR 0
      MAX_USER_CONNECTIONS 0;
 
--- Appliquer les changements
+-- Applique les changements
 FLUSH PRIVILEGES;
+
+-- Crée les bases de données si elles n'existent pas
+CREATE DATABASE IF NOT EXISTS LinesoftRH;
+CREATE DATABASE IF NOT EXISTS LinsoftAuthent;
